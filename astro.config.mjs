381a93fc7026416b6506e2ea7ce13ce13ca36f7a
David Nguyen <phuc10cdt2@gmail.com>
2026-06-11 21:19:40 +0700

    Add sitemap and SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx()],
+  site: "https://davidnd.pages.dev",
+  integrations: [mdx(), sitemap()],
 });
